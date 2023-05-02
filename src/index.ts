a9f601b37183a2da32dca9369b7ac6edf7aac595
Oleh Bohdan <oleh.bohdan@mgm-tp.com>
2023-05-02 15:49:28 +0200

    added callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "myname", age: 20 });
+const user1 = new User({ name: "Igor", age: 26 });
+user1.set({ name: "Sergey" });
 
 const mountpoint = document.createElement("div");
 mountpoint.classList.add("base");
@@ -8,7 +10,16 @@ mountpoint.style.height = "100vh";
 document.body.appendChild(mountpoint);
 
 // const b = document.getElementById("base");
-const elem = `Hello ${user.get("name")}`;
-const elem1 = `Hello ${user.get("age")}`;
+// const elem = `Hello ${user.get("name")}`;
+// const elem1 = `Hello ${user.get("age")}`;
+console.log(user);
+const users = [user, user1];
 
-mountpoint.append(elem, elem1);
+user.on("change", () => {});
+
+users.forEach((user) => {
+  const nameElem = document.createElement("div");
+  nameElem.textContent = `Hello ${user.get("name")} your age is - ${user.get("age")}`;
+
+  mountpoint.appendChild(nameElem);
+});
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,24 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+type Callback = () => void;
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
+    //@ts-ignore
     return this.data[propName];
+  }
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
   }
 }
