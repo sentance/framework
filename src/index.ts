2f8f64b5db9fdb397cdf890438b56263736e2803
Oleh Bohdan <oleh.bohdan@mgm-tp.com>
2023-06-02 09:58:56 +0200

    user model optimization, events model created

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
+import axios from "axios";
 import { User } from "./models/User";
 
-const user = new User({ name: "myname", age: 20 });
-const user1 = new User({ name: "Igor", age: 26 });
-user1.set({ name: "Sergey" });
+const newUser = new User({ name: "Serget", age: 51 });
 
-const mountpoint = document.createElement("div");
-mountpoint.classList.add("base");
-mountpoint.style.height = "100vh";
-document.body.appendChild(mountpoint);
-
-// const b = document.getElementById("base");
-// const elem = `Hello ${user.get("name")}`;
-// const elem1 = `Hello ${user.get("age")}`;
-console.log(user);
-const users = [user, user1];
-
-user.on("change", () => {});
-
-users.forEach((user) => {
-  const nameElem = document.createElement("div");
-  nameElem.textContent = `Hello ${user.get("name")} your age is - ${user.get("age")}`;
-
-  mountpoint.appendChild(nameElem);
-});
+newUser.save();
+// axios.get("http://localhost:3000/users/3").then((data) => {
+//   Object.values(data.data).map((item) => {
+//     if (item.name != undefined) {
+//       console.log(item.name);
+//     }
+//   });
+// });
+setTimeout(() => {
+  console.log(newUser);
+}, 4000);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,12 @@
+import axios, { AxiosResponse } from "axios";
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => void;
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -16,9 +16,21 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get("id")}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+  save(): void {
+    const id = this.get("id");
+
+    if (this.get("id")) {
+      //put
+      axios.put(`http://localhost:3000/users/${this.get("id")}`, this.data);
+    } else {
+      //post
+      axios.post("http://localhost:3000/users", this.data);
+    }
   }
 }
